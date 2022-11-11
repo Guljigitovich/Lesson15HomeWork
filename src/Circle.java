4fe0f77a34d337ba293d624e25f1798a45893d1f
Saparov Syimyk <guljigitovich.180698@gmai.com>
2022-11-11 14:59:02 +0600

    Commit

--- src/Circle.java
+++ src/Circle.java
@@ -10,12 +10,8 @@ public class Circle {
     public void setRadius(int radius) {
         this.radius = radius;
 
-    }public void getInfo (){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("San jaz: ");
-        System.out.println("Joop: "+area(scanner.nextInt()));
-        System.out.print("San jaz: ");
-        System.out.println("Joop: "+circumference(scanner.nextInt()));
+
+
     }
 
     public static String area(int number){
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Circle circle = new Circle();
-        circle.getInfo();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("San jaz: ");
+        System.out.println("Joop: "+Circle.area(scanner.nextInt()));
+        System.out.print("San jaz: ");
+        System.out.println("Joop: "+Circle.circumference(scanner.nextInt()));
+
 
     }
 }
